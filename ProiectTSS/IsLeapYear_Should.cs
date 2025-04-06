def5582df8cef6c0d3c8f009efc06a8682773c84
Tudor Butufei <tudor.butufei@gmail.com>
2025-04-06 14:39:40 +0300

    Add project files.

---
+++ ProiectTSS/IsLeapYear_Should.cs
@@ -0,0 +1,34 @@
+using ProiectTSSApplication;
+
+namespace ProiectTSSTests
+{
+    public class IsLeapYear_Should
+    {
+        private readonly Program _program;
+
+        public IsLeapYear_Should()
+        {
+            _program = new Program();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(200)]
+        [InlineData(50)]
+        public void IsLeapYear_ReturnFalse(int year)
+        {
+            var result = _program.IsLeapYear(year);
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(20)]
+        [InlineData(400)]
+        [InlineData(800)]
+        public void IsLeapYear_ReturnTrue(int year)
+        {
+            var result = _program.IsLeapYear(year);
+            Assert.True(result);
+        }
+    }
+}
---
+++ ProiectTSSApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ProiectTSSApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
