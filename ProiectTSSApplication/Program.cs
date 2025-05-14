bb8863e3c3eb56b3e5f5f135e755adfdd795f736
Tudor Butufei <tudor.butufei@gmail.com>
2025-05-14 14:11:58 +0300

    Update program and cleanup unused code

--- ProiectTSSApplication/Program.cs
+++ ProiectTSSApplication/Program.cs
@@ -1,201 +1,10 @@
-﻿using System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Reflection;
-using System.Numerics;
-
-namespace ProiectTSSApplication
+﻿namespace ProiectTSSApplication
 {
-    public enum TagClass
-    {
-        Universal = 0x00,
-        Application = 0x01,
-        ContextSpecific = 0x02,
-        Private = 0x03
-    }
-
-    public enum TagType
-    {
-        Primitive = 0x00,
-        Constructed = 0x01
-    }
-
-    public enum PrimitiveTagType
-    {
-        Boolean = 0x01,
-        Integer = 0x02,
-        BitString = 0x03,
-        OctetString = 0x04,
-        Null = 0x05,
-        ObjectIdentifier = 0x06
-    }
-
-    public enum ConstructedTagType
-    {
-        Sequence = 0x10
-    }
-
-
     public class Program
     {
-        public static readonly string[] tagTypeLabels = ["primitive", "constructed"];
-        public static readonly string[] tagClassLabels = ["Universal", "Application", "ContextSpecific", "Private"];
-
         static void Main(string[] args)
         {
 
-        }
-
-        public string GetTagInformation(TagClass tagClass, TagType tagType, byte tagNumber, int dataLen, ReadOnlySpan<byte> dataBuffer, ref int dataPosition)
-        {
-            if (tagType == TagType.Constructed && tagNumber == (byte) ConstructedTagType.Sequence)
-            {
-                return $"Sequence of length {dataLen}";
-            }
-            else if (tagType == TagType.Primitive)
-            {
-                if ((tagNumber == (byte)PrimitiveTagType.Boolean) && (dataLen == 1))
-                {
-                    return $"Boolean {(ReadByte(dataBuffer, ref dataPosition) == 0x00 ? "false" : "true")}";
-                }
-                else if ((tagNumber == (byte)PrimitiveTagType.Integer))
-                {
-                    // Copy signed big-endian data from DER encoding
-                    byte[] valueBytes = new byte[dataLen];
-                    Buffer.BlockCopy(dataBuffer.ToArray(), dataPosition, valueBytes, 0, dataLen);
-
-                    // .NET expects little-endian
-                    Array.Reverse(valueBytes);
-                    BigInteger intVal = new(valueBytes);
-
-                    return $"Integer {intVal}";
-                }
-                else if((tagNumber == (byte)PrimitiveTagType.BitString) && (dataLen > 1))
-                {
-                    // Get unused number of bits
-                    byte unusedBits = ReadByte(dataBuffer, ref dataPosition);
-                    if (unusedBits > 7)
-                    {
-                        throw new Exception("Invalid unused bits");
-                    }
-
-                    // Get value bytes string
-                    byte[] valueBytes = new byte[dataLen - 1];
-                    Buffer.BlockCopy(dataBuffer.ToArray(), dataPosition, valueBytes, 0, dataLen - 1);
-                    string bytesStr = BitConverter.ToString(valueBytes).Replace("-", " ");
-
-                    return $"Bit string with {unusedBits} unused bits and data {bytesStr}";
-                }
-                else if ((tagNumber == (byte)PrimitiveTagType.OctetString))
-                {
-                    // Get value bytes string
-                    byte[] valueBytes = new byte[dataLen];
-                    Buffer.BlockCopy(dataBuffer.ToArray(), dataPosition, valueBytes, 0, dataLen);
-                    string bytesStr = BitConverter.ToString(valueBytes).Replace("-", " ");
-
-                    return $"Octed string {bytesStr}";
-                }
-                else if ((tagNumber == (byte)PrimitiveTagType.Null) && (dataLen == 0))
-                {
-                    return $"Null";
-                }
-                else if ((tagNumber == (byte)PrimitiveTagType.ObjectIdentifier))
-                {
-                    // Decode first byte
-                    byte firstByte = ReadByte(dataBuffer, ref dataPosition);
-                    int value1 = firstByte / 40;
-                    int value2 = firstByte % 40;
-
-                    var oidParts = new List<long> { value1, value2 };
-
-                    // Decode base-128 integers
-                    int endPosition = dataPosition - 1 + dataLen;
-                    while (dataPosition < endPosition)
-                    {
-                        long value = 0;
-                        byte b;
-                        do
-                        {
-                            b = ReadByte(dataBuffer, ref dataPosition);
-                            value = ((int)value << 7) | (int)(b & 0x7F);
-                        } while ((b & 0x80) != 0);
-
-                        oidParts.Add(value);
-                    }
-
-                    return $"Object Identifier: {string.Join(".", oidParts.ToArray())}";
-                }
-            }
-
-            return $"{tagClassLabels[(int)tagClass]} {tagTypeLabels[(int)tagType]} tag: {tagNumber}, with data length {dataLen}";
-        }
-
-        /// <summary>
-        /// Parses the DER tag header from the given buffer and extracts the tag class, tag type, tag number, and the data length.
-        /// </summary>
-        /// <param name="buffer">
-        /// A read-only span containing the DER-encoded data, starting from the position of the tag header.
-        /// This buffer should contain the tag byte and the length of the associated data.
-        /// </param>
-        /// <param name="position">
-        /// A reference to the current position in the buffer, starting from the position of the tag header.
-        /// It is updated during the execution of the method to reflect the position after the tag byte and
-        /// length bytes are processed.
-        /// </param>
-        /// <param name="tagClass">
-        /// An output parameter that will hold the tag's class. This will be set to one of the following values from the <see cref="TagClass"/> enum:
-        /// <list type="bullet">
-        /// <item><description><see cref="TagClass.Universal"/> = 0x00</description></item>
-        /// <item><description><see cref="TagClass.Application"/> = 0x01</description></item>
-        /// <item><description><see cref="TagClass.ContextSpecific"/> = 0x02</description></item>
-        /// <item><description><see cref="TagClass.Private"/> = 0x03</description></item>
-        /// </list>
-        /// </param>
-        /// <param name="tagType">
-        /// An output parameter that will hold the tag's type. This will be set to one of the following values from the <see cref="TagType"/> enum:
-        /// <list type="bullet">
-        /// <item><description><see cref="TagType.Primitive"/> = 0x00</description></item>
-        /// <item><description><see cref="TagType.Constructed"/> = 0x01</description></item>
-        /// </list>
-        /// </param>
-        /// <param name="tagNumber">
-        /// An output parameter that will hold the tag number. This will be a value between 0 and 31, unless high-tag-number form is used.
-        /// In case of a high-tag-number, an exception is thrown.
-        /// </param>
-        /// <param name="dataLen">
-        /// An output parameter that will hold the data length (in bytes) for the encoded data following the tag. 
-        /// This value is calculated using the <see cref="GetDataLength"/> function.
-        /// </param>
-        /// <exception cref="Exception">
-        /// Thrown if the tag number is 31 (0x1F), indicating the use of high-tag-number form, which is unsupported
-        /// or if there is insufficient data in the buffer to read the full length.
-        /// </exception>
-        /// <remarks>
-        /// This method extracts information about the tag class, tag type, and tag number from the first byte of the tag header, 
-        /// and calculates the length of the encoded data using the following bytes. 
-        /// It ensures that the tag number is between 0 and 31, throwing an exception if the high-tag-number form is detected.
-        /// </remarks>
-        public void ParseTagHeader(ReadOnlySpan<byte> buffer, ref int position, out TagClass tagClass, out TagType tagType, out byte tagNumber, out int dataLen)
-        {
-            // Read first byte (tag)
-            byte tagByte = ReadByte(buffer, ref position);
-
-            // Extract Tag Class (bits 8-7)
-            tagClass = (TagClass)((tagByte >> 6) & 0x03);
-
-            // Extract Tag Type (Primitive / Constructed bit 6)
-            tagType = (TagType)((tagByte >> 5) & 0x01);
-
-            // Extract Tag Number (bits 5-1)
-            tagNumber = (byte)(tagByte & 0x1F);
-
-            // High-tag-number form not supported
-            if (tagNumber == 0x1F)
-            {
-                throw new Exception("High-tag-number form not supported");
-            }
-
-            // Get the length of the encoded data
-            dataLen = GetDataLength(buffer, ref position); 
         }
 
         /// <summary>
@@ -207,11 +16,16 @@ namespace ProiectTSSApplication
         /// <returns>The length of the subsequent value in bytes.</returns>
         /// <exception cref="Exception">
         /// Thrown if the length is encoded using an indefinite form (not allowed in DER),
-        /// or if there is insufficient data in the buffer to read the full length.
+        /// or if there is insufficient data in the buffer to read the full length
+        /// or if invalid position in the buffer
         /// </exception>
-        public int GetDataLength(ReadOnlySpan<byte> buffer, ref int position)
+        public long GetDataLength(byte firstByte, ReadOnlySpan<byte> buffer, ref int position)
         {
-            byte firstByte = ReadByte(buffer, ref position);
+            // Validate parameter position
+            if (position < 0 || position >= buffer.Length)
+            {
+                throw new Exception("Invalid position");
+            }
 
             // Check if single byte length 
             if ((firstByte & 0x80) == 0)
@@ -227,29 +41,20 @@ namespace ProiectTSSApplication
             }
 
             // Add each byte to the length
-            int length = 0;
+            long length = 0;
             for (int i = 0; i < numBytes; i++)
             {
-                length = (length << 8) | ReadByte(buffer, ref position);
+                if (position < buffer.Length)
+                {
+                    length = (length << 8) | buffer[position++];
+                }
+                else
+                {
+                    throw new Exception("Unexpected end of data");
+                }
             }
+
             return length;
-        }
-
-        /// <summary>
-        /// Reads a single byte from the buffer at the specified position and advances the position.
-        /// </summary>
-        /// <param name="buffer">The byte span containing DER-encoded data.</param>
-        /// <param name="position">A reference to the current read position in the buffer. It will be incremented by one.</param>
-        /// <returns>The byte at the current position.</returns>
-        /// <exception cref="Exception">Thrown if the position is beyond the end of the buffer.</exception>
-        public byte ReadByte(ReadOnlySpan<byte> buffer, ref int position)
-        {
-            if (position < buffer.Length)
-            {
-                return buffer[position++];
-            }
-
-            throw new Exception("Unexpected end of data");
         }
     }
 }
